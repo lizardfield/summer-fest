a4b6ee184684a41167a1715c18b52b457aaba613
Elizabeth Benefiel <lizardfield@gmail.com>
2015-03-26 02:01:08 -0500

    a bit of renaming for consistency and clarity, bottom scroll is populated according to loaded data

--- js/map.js
+++ js/map.js
@@ -36,22 +36,52 @@ $(window).load(function () {
     shadowAnchor: [12, 20]
   });
 
-  var placeMarker = function(d) {
-    L.marker([d.latitude,d.longitude], {icon: myIcon}).addTo(map);
+  var placeEvents = function(event) {
+    // add event marker to map
+    L.marker([event.latitude,event.longitude], {icon: myIcon})
+    .addEventListener("click", showEvent, event)
+    .addTo(map);
+
+    // set default image for bottom scroll
+    var eventImage = event.image;
+    if (eventImage === null) {
+      var eventImage = "http://4.bp.blogspot.com/-MzZCzWI_6Xc/UIUQp1qPfzI/AAAAAAAAHpA/OTwHCJSWFAY/s1600/cats_animals_kittens_cat_kitten_cute_desktop_1680x1050_hd-wallpaper-753974.jpeg";
+    };
+
+    // add event to bottom scroll
+    $("#event-scroll").append("<li class='event'><a href=#><img src='" + eventImage + "' alt='" + event.name + "'><h2>" + event.name + "<br>" + event.date + "</h2></a></li>");
   }
+
+  // image URL is incorrect
+
+  var placeSubevents = function(subevent) {
+    $("#subevent-scroll").append("<li>" + subevent.name + "</li>");
+  }
+
+  var showEvent = function(details) {
+    console.log(this);
+  }
+
+//  var populateSidebar = function(d) {
+//    $("#subevent-scroll").empty();
+//    getData("http://127.0.0.1:8000/photoMap/api/events/" + );
+//  }
+// how to know total # of subevents? does there need to be a subevent array in api/maps?
 
   var populateMap = function(d) {
-  // replace titles
-  $(".interactive-title").html(d.name);
-  document.title = d.name;
-  $(".sidebar-title").html("Filter by " + d.subevent_type);
+    // replace titles
+    $(".interactive-title").html(d.name);
+    document.title = d.name;
+    $(".sidebar-title").html("Filter by " + d.subevent_type);
 
-  // populate map w/ markers
-  for (var i = 0; i < d.events.length; i++) {
-    console.log(d.events[i]);
-    getData("http://127.0.0.1:8000/photoMap/api/events/" + d.events[i], placeMarker); // run
+    $("#event-scroll").empty();
+
+    // populate map w/ markers
+    for (var i = 0; i < d.events.length; i++) {
+      console.log(d.events[i]);
+      getData("http://127.0.0.1:8000/photoMap/api/events/" + d.events[i], placeEvents);
+    }
   }
-}
 
   getData("http://127.0.0.1:8000/photoMap/api/maps/1/", populateMap);
 });
